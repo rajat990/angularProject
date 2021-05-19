fefb2fe225786bcd672d1ebcce293a9b634e1dae
rajat990 <50400854+rajat990@users.noreply.github.com>
2021-05-19 10:09:38 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello{
     public static void main(String args[]){
 
         System.out.println("hello world");
-        System.out.println("hoii rajat sahcan");
+        System.out.println("hoii ");
         System.out.println("hoii rajat sachan");
     }
 }
